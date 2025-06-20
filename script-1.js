696f36185695b67470600a17915aba789eee74fa
rakha <abdulkhutsaev@mail.ru>
2025-06-20 15:44:21 +0300

    change

---
+++ script-1.js
@@ -0,0 +1,24 @@
+let age = 10;
+age = 15;
+
+console.log(age)
+
+let red = 20;
+let redd = "20";
+let reddd = red + redd;
+
+console.log(reddd)
+
+const cd = "hello brother";
+
+console.log(cd)
+
+let l = "name";
+prompt(l)
+
+let wh = "phone";
+prompt(wh)
+
+let hh = "address";
+prompt(hh)
+
